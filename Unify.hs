6634d8315f29e26869993d10c111ea9f96deef25
Superstar64 <Hexagonalstar64@gmail.com>
2023-02-13 17:06:16 -0500

    remove recurse factorization, instead just use algebraic normal form

--- Unify.hs
+++ Unify.hs
@@ -3,7 +3,7 @@
 module Unify where
 
 import Control.Monad.Trans.State
-import Data.Foldable (fold, traverse_)
+import Data.Foldable (traverse_)
 import Data.List (intercalate)
 import Data.Set (Set, union, (\\))
 import qualified Data.Set as Set
@@ -37,122 +37,83 @@ instance Ord x => BooleanRing (Space x) where
     pure $ Space $ Set.singleton $ Set.union a' b'
   Space a |+| Space b = Space $ (a \\ b) `union` (b \\ a)
 
-data Term
-  = Constant (Space String)
-  | -- xa + b
-    -- (x `and` a) `xor` b
-    -- x not in free(a,b)
-    Polynomial String Term Term
-  deriving (Show)
-
-algebraic :: Term -> Space String
-algebraic (Constant c) = c
-algebraic (Polynomial x e1 e2) = Space (Set.singleton $ Set.singleton x) |*| algebraic e1 |+| algebraic e2
+factor :: Ord x => x -> Space x -> (Space x, Space x)
+factor x (Space e) =
+  ( Space $ Set.map (Set.delete x) $ Set.filter (Set.member x) e,
+    Space $ Set.filter (Set.notMember x) e
+  )
 
 pretty :: (x -> String) -> Space x -> String
-pretty f (Space c) | Set.null c = "0"
+pretty _ (Space c) | Set.null c = "0"
 pretty f (Space c) = intercalate " + " $ map product (Set.toList c)
   where
     product c | Set.null c = "1"
     product c = intercalate "*" $ map f $ Set.toList c
 
-polynomial :: String -> Term -> Term -> Term
-polynomial _ (Constant (Space a)) e | Set.null a = e
-polynomial x a b = Polynomial x a b
+data Variable
+  = Flexible String
+  | Constant String
+  deriving (Show, Eq, Ord)
 
-instance BooleanRing Term where
-  zero = Constant $ zero
-  one = Constant $ one
-  Constant c |*| Polynomial x a b = polynomial x (a |*| Constant c) (b |*| Constant c)
-  Polynomial x a b |*| Constant c = polynomial x (Constant c |*| a) (Constant c |*| b)
-  Constant a |*| Constant b = Constant $ a |*| b
-  Polynomial x a b |*| Polynomial x' c d | x == x' = polynomial x (f' |+| i' |+| o') l
-    where
-      f' = a |*| c
-      o' = a |*| d
-      i' = b |*| c
-      l = b |*| d
-  Polynomial x a b |*| Polynomial y c d = f |+| o |+| i |+| l
-    where
-      f = polynomial x (polynomial y (m |*| (p |+| q) |+| n |*| (p |+| q)) zero) zero
-        where
-          (m, n) = factor y a
-          (p, q) = factor x c
-      o = polynomial x (a |*| (m |+| n)) zero
-        where
-          (m, n) = factor x d
-      i = polynomial y ((m |+| n) |*| c) zero
-        where
-          (m, n) = factor y b
-      l = b |*| d
-  Constant a |+| Constant b = Constant $ a |+| b
-  Constant c |+| Polynomial x a b = polynomial x a (Constant c |+| b)
-  Polynomial x a b |+| Constant c = polynomial x a (b |+| Constant c)
-  Polynomial x a b |+| Polynomial x' c d | x == x' = polynomial x (a |+| c) (b |+| d)
-  Polynomial x a b |+| Polynomial y c d = polynomial x (polynomial y (e |+| i) (k |+| f)) (polynomial y (g |+| j) (l |+| h))
-    where
-      (e, f) = factor y a
-      (g, h) = factor y b
-      (i, j) = factor x c
-      (k, l) = factor x d
+variable x = Space $ Set.singleton $ Set.singleton $ Flexible x
 
-factor :: String -> Term -> (Term, Term)
-factor _ (Constant c) = (zero, Constant c)
-factor x (Polynomial x' a b) | x == x' = (a, b)
-factor x (Polynomial y a b) = (polynomial y c e, polynomial y d f)
+constant x = Space $ Set.singleton $ Set.singleton $ Constant x
+
+prettyVariable (Flexible x) = x
+prettyVariable (Constant x) = x
+
+substitute :: (String, Space Variable) -> Space Variable -> Space Variable
+substitute (x, ex) (Space e) = foldr (|+|) zero (map mul $ Set.toList e)
   where
-    (c, d) = factor x a
-    (e, f) = factor x b
+    mul e = foldr (|*|) one (map apply $ Set.toList e)
+      where
+        apply (Flexible x') | x == x' = ex
+        apply e = Space $ Set.singleton $ Set.singleton e
 
-substitute :: (String, Term) -> Term -> Term
-substitute (x, ex) (Polynomial x' a b) | x == x' = ex |*| a |+| b
-substitute (x, ex) (Polynomial x' a b) = variable x' |*| substitute (x, ex) a |+| substitute (x, ex) b
-substitute _ (Constant c) = Constant c
+freeVariables :: Space Variable -> Set String
+freeVariables (Space e) = Set.fromList $ do
+  e' <- Set.toList e
+  e'' <- Set.toList e'
+  case e'' of
+    Flexible x -> [x]
+    Constant _ -> []
 
-variable :: String -> Term
-variable x = Polynomial x one zero
-
-constant :: String -> Term
-constant x = Constant $ Space $ Set.singleton (Set.singleton x)
-
-fresh :: StateT Int IO Term
+fresh :: StateT Int IO (Space Variable)
 fresh = do
   i <- get
   put (i + 1)
   pure $ variable ("x" ++ show i)
 
-solve' :: Term -> StateT Int IO [(String, Term)]
-solve' (Constant (Space e)) | Set.null e = pure []
-solve' (Polynomial x t1 t2) = do
-  θ <- solve' (inc t1 |*| t2)
-  u <- fresh
-  let apply e = foldr substitute e θ
-  pure $ (x, apply $ inc t1 |*| u |+| t2) : θ
-solve' (Constant c) = fail $ "unification error: " ++ show c
-
 run :: StateT Int IO e -> IO e
 run e = evalStateT e 0
 
-solve :: Term -> IO ()
-solve e = run (solve' e) >>= traverse_ (\(x, e) -> putStrLn $ x ++ " = " ++ pretty id (algebraic e))
+solveFor :: [String] -> Space Variable -> StateT Int IO [(String, Space Variable)]
+solveFor _ (Space e) | Set.null e = pure []
+solveFor (x : xs) e = do
+  let (t1, t2) = factor (Flexible x) e
+  θ <- solveFor xs (inc t1 |*| t2)
+  u <- fresh
+  let apply e = foldr substitute e θ
+  pure $ (x, apply $ inc t1 |*| u |+| t2) : θ
+solveFor [] e = fail $ "unification error " ++ pretty prettyVariable e
 
-satify :: Term -> IO ()
+solve :: Space Variable -> IO ()
+solve e = do
+  θ <- run (solveFor (Set.toList $ freeVariables e) e)
+  traverse_ (\(x, e) -> putStrLn $ x ++ " = " ++ pretty prettyVariable e) θ
+
+satify :: Space Variable -> IO ()
 satify e = solve (inc e)
 
-unify :: Term -> Term -> IO ()
+unify :: Space Variable -> Space Variable -> IO ()
 unify e1 e2 = solve (e1 |+| e2)
 
 test :: IO ()
 test = unify (variable "x" |*| constant "a") (constant "a")
 
-or' :: Term -> Term -> Term
-or' x y = x |+| y |+| x |*| y
-
-all' :: (Foldable t) => t Term -> Term
 all' = foldr (|*|) one
 
-any' :: (Foldable t) => t Term -> Term
-any' = foldr (or') zero
+any' = foldr (\x y -> x |+| y |+| x |*| y) zero
 
 adder c x y cout s = adder' [c, x, y, cout, s]
   where
