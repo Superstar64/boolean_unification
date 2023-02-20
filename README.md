274dceb206a1e11b473f7e204d656804b642e05b
Superstar64 <Hexagonalstar64@gmail.com>
2023-02-20 14:21:10 -0500

    new method to solve sets of problems

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Boolean_Unification
-This is an implementation of Boolean Unification in Haskell. This is heavily based of ["Embedding Boolean Expressions into Logic Programming"](https://core.ac.uk/download/pdf/82206645.pdf). 
+This is an implementation of Boolean Unification in Haskell.
+The is somewhat based of ["Embedding Boolean Expressions into Logic Programming"](https://core.ac.uk/download/pdf/82206645.pdf).
+
+This unifier takes the algorithm from the paper and applies a simple heuristic to solve sets of problems in algebraic normal form.
+The variable that appears in least number of problem is choosen to be solved for.
+The problems are combined into a single problem using boolean logic then the method in from paper is applied.
+This is appiled recursively until all varibales are solved.
--- Unify.hs
+++ Unify.hs
@@ -2,9 +2,8 @@
 
 module Unify where
 
-import Control.Monad.Trans.State
 import Data.Foldable (traverse_)
-import Data.List (intercalate)
+import Data.List (intercalate, sortOn)
 import Data.Set (Set, union, (\\))
 import qualified Data.Set as Set
 
@@ -55,8 +54,10 @@ data Variable
   | Constant String
   deriving (Show, Eq, Ord)
 
+variable :: String -> Space Variable
 variable x = Space $ Set.singleton $ Set.singleton $ Flexible x
 
+constant :: String -> Space Variable
 constant x = Space $ Set.singleton $ Set.singleton $ Constant x
 
 prettyVariable (Flexible x) = x
@@ -78,49 +79,68 @@ freeVariables (Space e) = Set.fromList $ do
     Flexible x -> [x]
     Constant _ -> []
 
-fresh :: StateT Int IO (Space Variable)
-fresh = do
-  i <- get
-  put (i + 1)
-  pure $ variable ("x" ++ show i)
-
-run :: StateT Int IO e -> IO e
-run e = evalStateT e 0
-
-solveFor :: [String] -> Space Variable -> StateT Int IO [(String, Space Variable)]
-solveFor _ (Space e) | Set.null e = pure []
-solveFor (x : xs) e = do
+solveStep :: String -> Space Variable -> (Space Variable, Space Variable)
+solveStep x e =
   let (t1, t2) = factor (Flexible x) e
-  θ <- solveFor xs (inc t1 |*| t2)
-  u <- fresh
-  let apply e = foldr substitute e θ
-  pure $ (x, apply $ inc t1 |*| u |+| t2) : θ
-solveFor [] e = fail $ "unification error " ++ pretty prettyVariable e
+   in (inc t1 |*| t2, inc t1 |*| variable x |+| t2)
 
-solve :: Space Variable -> IO ()
-solve e = do
-  θ <- run (solveFor (Set.toList $ freeVariables e) e)
-  traverse_ (\(x, e) -> putStrLn $ x ++ " = " ++ pretty prettyVariable e) θ
+combine :: BooleanRing e => [e] -> e
+combine problem = inc $ foldr (|*|) one (map inc problem)
 
-satify :: Space Variable -> IO ()
-satify e = solve (inc e)
+split :: String -> [Space Variable] -> ([Space Variable], [Space Variable])
+split x problem = (filter (\e -> Set.member x (freeVariables e)) problem, filter (\e -> Set.notMember x (freeVariables e)) problem)
 
-unify :: Space Variable -> Space Variable -> IO ()
-unify e1 e2 = solve (e1 |+| e2)
+solveAll :: MonadFail f => [String] -> [Space Variable] -> f [(String, Space Variable)]
+solveAll xs problem = case filter (\(Space e) -> not $ Set.null e) problem of
+  [] -> pure []
+  problem | [] <- xs -> fail $ "unification error" ++ show problem
+  problem -> do
+    let (x, (simple, problem')) = head $ sortOn (length . fst . snd) $ map (\x -> (x, split x problem)) xs
+        (simple', answer) = solveStep x (combine simple)
+     in ((x, answer) :) <$> solveAll (filter (/= x) xs) (simple' : problem')
 
-test :: IO ()
-test = unify (variable "x" |*| constant "a") (constant "a")
+renameAnswers :: Int -> [(String, Space Variable)] -> [(String, Space Variable)]
+renameAnswers _ [] = []
+renameAnswers i ((x, e) : θ) =
+  (x, substitute (x, variable $ "_x" ++ show i) e) : renameAnswers (i + 1) θ
 
-all' = foldr (|*|) one
+backSubstitute :: [(String, Space Variable)] -> [(String, Space Variable)]
+backSubstitute [] = []
+backSubstitute ((x, e) : θ) =
+  let θ' = backSubstitute θ
+   in (x, foldr substitute e θ') : θ'
 
-any' = foldr (\x y -> x |+| y |+| x |*| y) zero
+printAnswers :: [(String, Space Variable)] -> IO ()
+printAnswers =
+  traverse_ (\(x, e) -> putStrLn $ x ++ " = " ++ pretty prettyVariable e)
 
-adder c x y cout s = adder' [c, x, y, cout, s]
+solveAllIO e = do
+  θ <- solveAll (Set.toList $ foldMap freeVariables e) e
+  printAnswers $ backSubstitute $ (renameAnswers 0 θ)
+
+satifyAllIO :: [Space Variable] -> IO ()
+satifyAllIO e = solveAllIO (map inc e)
+
+solveIO :: Space Variable -> IO ()
+solveIO e = satifyAllIO [e]
+
+satifyIO :: Space Variable -> IO ()
+satifyIO e = solveIO (inc e)
+
+unifyIO :: Space Variable -> Space Variable -> IO ()
+unifyIO e1 e2 = solveIO (e1 |+| e2)
+
+testBasic :: IO ()
+testBasic = unifyIO (variable "x" |*| constant "a") (constant "a")
+
+testAdder :: IO ()
+testAdder = satifyIO $ adder [constant "c", constant "x", constant "y", variable "Cout", variable "S"]
   where
-    adder' terms = any' $ map (all' . zipWith apply [0 ..]) adderRaw
+    adder terms = any' $ map (all' . zipWith apply [0 ..]) adderRaw
       where
         apply index '0' = inc $ terms !! index
         apply index '1' = terms !! index
+        apply _ _ = error "bad number"
     adderRaw =
       [ "00000",
         "00101",
@@ -131,5 +151,52 @@ adder c x y cout s = adder' [c, x, y, cout, s]
         "11010",
         "11111"
       ]
+    any' = foldr (\x y -> x |+| y |+| x |*| y) zero
+    all' = foldr (|*|) one
 
-testAdder = satify $ adder (constant "c") (constant "x") (constant "y") (variable "Cout") (variable "S")
+testRedundant =
+  solveAllIO
+    [ or (variable "v3") (variable "v5") |+| or (variable "v14") (variable "v5"),
+      or (variable "v14") (variable "v5") |+| or (variable "v16") (variable "v5"),
+      or (variable "v16") (variable "v5") |+| or (variable "v18") (variable "v5"),
+      or (variable "v18") (variable "v5") |+| or (variable "v20") (variable "v5"),
+      or (variable "v20") (variable "v5") |+| or (variable "v22") (variable "v5"),
+      or (variable "v22") (variable "v5") |+| or (variable "v24") (variable "v5"),
+      or (variable "v24") (variable "v5") |+| or (variable "v26") (variable "v5"),
+      or (variable "v26") (variable "v5") |+| or (variable "v28") (variable "v5"),
+      or (variable "v28") (variable "v5") |+| or (variable "v30") (variable "v5"),
+      or (variable "v30") (variable "v5") |+| or (variable "v32") (variable "v5"),
+      or (variable "v32") (variable "v5") |+| or (variable "v34") (variable "v5"),
+      or (variable "v34") (variable "v5") |+| or (variable "v36") (variable "v5"),
+      or (variable "v36") (variable "v5") |+| or (variable "v38") (variable "v5"),
+      or (variable "v38") (variable "v5") |+| or (variable "v40") (variable "v5"),
+      or (variable "v40") (variable "v5") |+| or (variable "v42") (variable "v5"),
+      variable "v42" |+| or (variable "v43") (variable "v5"),
+      variable "v40" |+| or (variable "v41") (variable "v5"),
+      variable "v38" |+| or (variable "v39") (variable "v5"),
+      variable "v36" |+| or (variable "v37") (variable "v5"),
+      variable "v34" |+| or (variable "v35") (variable "v5"),
+      variable "v32" |+| or (variable "v33") (variable "v5"),
+      variable "v30" |+| or (variable "v31") (variable "v5"),
+      variable "v28" |+| or (variable "v29") (variable "v5"),
+      variable "v26" |+| or (variable "v27") (variable "v5"),
+      variable "v24" |+| or (variable "v25") (variable "v5"),
+      variable "v22" |+| or (variable "v23") (variable "v5"),
+      variable "v20" |+| or (variable "v21") (variable "v5"),
+      variable "v18" |+| or (variable "v19") (variable "v5"),
+      variable "v16" |+| or (variable "v17") (variable "v5"),
+      variable "v14" |+| or (variable "v15") (variable "v5"),
+      variable "v3" |+| or (variable "v11") (variable "v5")
+    ]
+  where
+    or x y = x |+| y |+| x |*| y
+
+testRigid =
+  solveAllIO
+    [ constant "A" |+| or (variable "v6") (variable "v8"),
+      (constant "A") |*| (constant "C") |+| variable "v20",
+      (constant "A") |*| (constant "B") |+| variable "v8",
+      or (variable "v6") (variable "v8") |+| or (variable "v18") (variable "v20")
+    ]
+  where
+    or x y = x |+| y |+| x |*| y
